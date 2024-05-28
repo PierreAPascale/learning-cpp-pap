b38951d275affd071425ef99104b9c675169eac1
PierreAPascale <pierreapascale@gmail.com>
2024-05-28 21:55:08 +0000

    Notes rewritten

---
+++ src/notes.cpp
@@ -0,0 +1,39 @@
+/*
+C++ 
+OOO -> based on "Models". You can define an object in many ways, choose what you need to: you don't need a car's number of seats if you're creating racecars
+but you need it for a car retailer.
+
+ENCAPSULATION
+Only include information about themselves.
+Don't include gas price in the car; the gas station would have it.
+
+CLASSES
+Classes contain two types of members: data + function
+Car.make, Car.paint
+Each instance = an object of the class (3 car objects)
+
+
+ACCESS
+Private : accessible only by itself, not even inherited classes
+Public : accessible by any other part of the code
+Protected : accessible by inherited classes
+
+INHERITANCE AND POLYMORPHISM
+Subclasses (class based on another) and superclasses (the class it inherits from)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
