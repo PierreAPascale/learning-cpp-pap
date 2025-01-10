52c9d7c45958152dd33873725fa362a0cec17095
PierreAPascale <pierreapascale@gmail.com>
2025-01-10 22:37:00 +0000

    Update with 2nd Tutorial : memory Structure, pointers

--- src/Ch00/CodeDemo.cpp
+++ src/Ch00/CodeDemo.cpp
@@ -22,7 +22,7 @@ int main(){
 */
 
 
-
+/*
 
 // C++ program to point address 
 // of a pointer 
@@ -52,4 +52,21 @@ int main()
              &ptr; 
 
     cout << endl << endl;
+}
+*/
+
+#include <iostream>
+ 
+int main()
+{
+    int n1 = 1;
+    int n2 = ++n1;
+    int n3 = ++ ++n1;
+    int n4 = n1++;
+//  int n5 = n1++ ++;   // error
+//  int n6 = n1 + ++n1; // undefined behavior
+    std::cout << "n1 = " << n1 << '\n'
+              << "n2 = " << n2 << '\n'
+              << "n3 = " << n3 << '\n'
+              << "n4 = " << n4 << '\n';
 }
